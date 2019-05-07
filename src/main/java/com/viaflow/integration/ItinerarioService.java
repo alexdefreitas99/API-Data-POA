96bf02e1bc7e02e3a91ebfecc8ed0ede106b2143
Alex Freitas <alex.freitas@viaflow.net>
2019-05-07 09:55:09 -0300

    Add coordenadas

--- src/main/java/com/viaflow/integration/ItinerarioService.java
+++ src/main/java/com/viaflow/integration/ItinerarioService.java
@@ -1,6 +1,7 @@
 package com.viaflow.integration;
 
 import java.util.Arrays;
+import java.util.Set;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -10,8 +11,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.viaflow.document.Coordenadas;
 import com.viaflow.document.Itinerario;
 
 @Service
@@ -28,6 +31,19 @@ public class ItinerarioService {
 			.getAsJsonObject();	
 		Itinerario itinerario = new Itinerario();
 		itinerario =gson.fromJson(itinerarioJson, Itinerario.class);
+		Set<String> chaves = itinerarioJson.keySet();
+		System.out.println(chaves);
+		for (String chave : chaves) {
+			try {
+				int val = 0;
+				val = Integer.parseInt(chave);
+				JsonObject jsonElement = itinerarioJson.getAsJsonObject(Integer.toString(val));
+				Coordenadas coor = new Coordenadas();
+				coor.setLat(jsonElement.get("lat").getAsString());
+				coor.setLng(jsonElement.get("lng").getAsString());
+				itinerario.getCoordenadas().add(coor);
+			} catch (NumberFormatException e) {}			
+		}		
 		return itinerario;
 	}
 }
