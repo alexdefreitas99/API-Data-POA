5d7c86232a2ecf4b3e9c40416d0f0b2aa005e500
alex.freitas <alex.freitas@DESKTOP-13I5IT0.viaflow.net>
2019-05-16 14:48:38 -0300

    teste

--- src/main/java/com/viaflow/OnibusApiApplication.java
+++ src/main/java/com/viaflow/OnibusApiApplication.java
@@ -1,12 +1,6 @@
 package com.viaflow;
 
 import org.springframework.boot.SpringApplication;
-
-import com.mongodb.ConnectionString;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.MongoClientSettings;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
@@ -15,14 +9,6 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 public class OnibusApiApplication {
 
 	public static void main(String[] args) {
-		ConnectionString connString = new ConnectionString(
-				"mongodb+srv://viaflow:viaflow123@cluster0-vkfum.mongodb.net/integration?retryWrites=true");
-		MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(connString).retryWrites(true)
-				.build();
-		MongoClient mongoClient = MongoClients.create(settings);
-		MongoDatabase database = mongoClient.getDatabase("integration");
-
-		SpringApplication.run(OnibusApiApplication.class, args);
-
+		SpringApplication.run(OnibusApiApplication.class, args);		
 	}
 }
