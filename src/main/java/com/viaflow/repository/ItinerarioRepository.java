caefa8a4c3d4085a381b5be2788aef0d11df0a1f
alex.freitas <alex.freitas@DESKTOP-13I5IT0.viaflow.net>
2019-05-17 09:30:13 -0300

    removed import

--- src/main/java/com/viaflow/repository/ItinerarioRepository.java
+++ src/main/java/com/viaflow/repository/ItinerarioRepository.java
@@ -4,7 +4,6 @@ package com.viaflow.repository;
 import java.util.List;
 
 import org.springframework.data.geo.Distance;
-import org.springframework.data.geo.GeoResults;
 import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
